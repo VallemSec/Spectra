f0bcb506ae6d6852e6d17c89dfea392aed8ae4e4
Bavo <bavo.famknol@gmail.com>
2024-09-18 11:11:07 +0200

    Running discovery services from config works

--- orchestratorService/main.go
+++ orchestratorService/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"os"
 
 	"gopkg.in/yaml.v2"
 )
+
+type JSONbody struct {
+	Target string `json:"target"`
+}
 
 type Config struct {
 	DiscoveryRunners []string                `yaml:"discovery_runners"`
@@ -21,7 +28,7 @@ type RunnerConfig struct {
 	Image         string              `yaml:"image"`
 	ImageVersion  string              `yaml:"image_version"`
 	ParserPlugin  string              `yaml:"parser_plugin"`
-	DecodyRule    string              `yaml:"decody_rule,omitempty"`
+	DecodyRule    []string            `yaml:"decody_rule,omitempty"`
 	Results       map[string][]string `yaml:"results"`
 }
 
@@ -33,18 +40,77 @@ func main() {
 			return
 		}
 
+		jsonBody := JSONbody{}
+		err = json.NewDecoder(r.Body).Decode(&jsonBody)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
 		var config Config
 		err = yaml.Unmarshal(yamlFile, &config)
 		if err != nil {
 			log.Fatalf("Unmarshal: %v", err)
 		}
 
+		var discoveryRunners []RunnerConfig
+
+		for _, runner := range config.DiscoveryRunners {
+			discoveryRunners = append(discoveryRunners, config.Runners[runner])
+		}
+
 		// Use the config variable as needed
-		log.Println(config)
+		for _, runner := range discoveryRunners {
+			// replace the target in the cmdargs
+			for i, arg := range runner.CmdArgs {
+				if arg == "{{req_domain}}" {
+					runner.CmdArgs[i] = jsonBody.Target
+				}
+			}
+
+			_, err := runDockerService(runner)
+			if err != nil {
+				log.Println(err)
+			}
+		}
 	})
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func runDockerService(config RunnerConfig) (string, error) {
+	type runnerJSON struct {
+		ContainerName    string   `json:"containerName"`
+		ContainerTag     string   `json:"containerTag"`
+		ContainerCommand []string `json:"containerCommand"`
+	}
+
+	configJSON := runnerJSON{
+		ContainerName:    config.Image,
+		ContainerTag:     config.ImageVersion,
+		ContainerCommand: config.CmdArgs,
+	}
+
+	jsonValue, err := json.Marshal(configJSON)
+	if err != nil {
+		return "", err
+	}
+
+	// send the request to the dockerRunnerService
+	resp, err := http.Post("http://localhost:8008", "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		return "", err
+	}
+
+	// read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	// return the response body
+	return string(body), nil
 }
