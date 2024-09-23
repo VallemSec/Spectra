c8db2a1e43d8720038fee8ea3a39e58391f19cf0
Luka de Jong <lukadejong2002@gmail.com>
2024-09-23 11:00:18 +0200

    Fixed auto tls run

--- decodyService/main.py
+++ decodyService/main.py
@@ -20,4 +20,4 @@ app.register_blueprint(endpoints.generate_app)
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5001, debug=True,
-            ssl_context="adhoc" if os.getenv("RUN_TLS", "1") else None)
+            ssl_context="adhoc" if os.getenv("RUN_TLS", "0") == "1" else None)
