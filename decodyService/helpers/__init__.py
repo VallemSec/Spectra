3326472b486d83b5b6d82cc5e08e0d684f4dbf0c
Luka de Jong <lukadejong2002@gmail.com>
2024-09-26 10:29:43 +0200

    Optimized code to 10/10 output from pylint

--- decodyService/helpers/__init__.py
+++ decodyService/helpers/__init__.py
@@ -1,1 +1,2 @@
+# pylint: disable=missing-module-docstring
 from helpers.eval import safe_eval
--- decodyService/helpers/eval.py
+++ decodyService/helpers/eval.py
@@ -1,25 +1,36 @@
+# pylint: disable=eval-used
+"""
+helpers.eval contains all the code required to parse
+boolean expressions written in python and return their result.
+
+Example:
+    x = 5
+    result = helpers.eval.safe_eval("x == 5", {"x": x})
+    print(result)
+"""
 import ast
 
 
 def safe_eval(expr, variables=None) -> bool:
     """
     This function evaluates a boolean expression using `eval`.
-    It is known that `eval` is unsafe, but the use of it here is justified since there will
-    be no unauthorized input.
+    It is known that `eval` is unsafe, but the use of it here is
+    justified since there will be no unauthorized input.
     :param expr: The boolean expression to be evaluated
-    :param variables: A dictionary with the variable name as the key and the value as value
+    :param variables: A dictionary with the variable name as the key
+    and the value as value
     :return: The result of the given expression
     """
     try:
         # Parse the expression
-        tree = ast.parse(expr, mode='eval')
+        tree = ast.parse(expr, mode="eval")
 
         # Compile and evaluate the expression safely
-        code = compile(tree, '<string>', 'eval')
+        code = compile(tree, "<string>", "eval")
         result = eval(code, {"__builtins__": {}}, variables)
         if not isinstance(result, bool):
-            raise ValueError(f"Evaluated statement is not a boolean")
+            raise ValueError("Evaluated statement is not a boolean")
         return result
 
     except Exception as e:
-        raise ValueError(f"Error evaluating expression: {e}")
+        raise ValueError(f"Error evaluating expression: {e}") from e
