1a7c1cb615234143ff15396e07a2480171889f02
Luka de Jong <lukadejong2002@gmail.com>
2024-09-27 07:27:57 +0200

    Added a generate endpoint to return data from keydb

--- decodyService/endpoints/generate.py
+++ decodyService/endpoints/generate.py
@@ -1,9 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, g
+import json
+import keydb
 
 
 generate_app = Blueprint("generate_app", __name__)
 
 
-@generate_app.get("/generate")
-def generate_endpoint():
-    pass
+@generate_app.get("/generate/<request_id>")
+def generate_endpoint(request_id: str):
+    g.keydb_conn: keydb.KeyDB
+    explanations_str = g.keydb_conn.get(f"{request_id}-explanations")
+    return json.loads(explanations_str), 200
