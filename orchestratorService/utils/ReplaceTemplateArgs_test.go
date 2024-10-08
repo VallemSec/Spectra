26e59dbcd99a6caa75ef9ac817e18513ebd55dd8
Bavo <bavo.famknol@gmail.com>
2024-10-08 12:11:39 +0200

    Fixed the tests and wrote for pass res

--- orchestratorService/utils/ReplaceTemplateArgs_test.go
+++ orchestratorService/utils/ReplaceTemplateArgs_test.go
@@ -7,17 +7,23 @@ import (
 func TestReplaceTemplateArgsWithoutReplaceables(t *testing.T) {
 	args := []string{"arg1", "arg2", "arg3"}
 	target := "example.com"
-	expected := []string{"arg1", "arg2", "arg3"}
+	expected := [][]string{args}
 
-	result := ReplaceTemplateArgs(args, target)
+	result := ReplaceTemplateArgs(args, target, nil)
 
 	if len(result) != len(expected) {
 		t.Errorf("expected %v, got %v", expected, result)
 	}
 
 	for i, v := range result {
-		if v != expected[i] {
+		if len(v) != len(expected[i]) {
 			t.Errorf("expected %v, got %v", expected, result)
+		}
+
+		for j, arg := range v {
+			if arg != expected[i][j] {
+				t.Errorf("expected %v, got %v", expected, result)
+			}
 		}
 	}
 }
@@ -25,17 +31,73 @@ func TestReplaceTemplateArgsWithoutReplaceables(t *testing.T) {
 func TestReplaceTemplateArgsWithReqdomain(t *testing.T) {
 	args := []string{"arg1", "{{req_domain}}", "arg3"}
 	target := "example.com"
-	expected := []string{"arg1", "example.com", "arg3"}
+	expected := [][]string{{"arg1", "example.com", "arg3"}}
 
-	result := ReplaceTemplateArgs(args, target)
+	result := ReplaceTemplateArgs(args, target, nil)
 
 	if len(result) != len(expected) {
 		t.Errorf("expected %v, got %v", expected, result)
 	}
 
 	for i, v := range result {
-		if v != expected[i] {
+		if len(v) != len(expected[i]) {
 			t.Errorf("expected %v, got %v", expected, result)
+		}
+
+		for j, arg := range v {
+			if arg != expected[i][j] {
+				t.Errorf("expected %v, got %v", expected, result)
+			}
+		}
+	}
+}
+
+func TestReplaceTemplateArgsWithPassString(t *testing.T) {
+	args := []string{"arg1", "{{pass_results}}", "arg3"}
+	target := "example.com"
+	results := []string{"result1", "result2"}
+	expected := [][]string{{"arg1", "result1 result2", "arg3"}}
+
+	result := ReplaceTemplateArgs(args, target, results)
+
+	if len(result) != len(expected) {
+		t.Errorf("expected %v, got %v", expected, result)
+	}
+
+	for i, v := range result {
+		if len(v) != len(expected[i]) {
+			t.Errorf("expected %v, got %v", expected, result)
+		}
+
+		for j, arg := range v {
+			if arg != expected[i][j] {
+				t.Errorf("expected %v, got %v", expected, result)
+			}
+		}
+	}
+}
+
+func TestReplaceTemplateArgsWithPassArray(t *testing.T) {
+	args := []string{"arg1", "{{[pass_results]}}", "arg3"}
+	target := "example.com"
+	results := []string{"result1", "result2"}
+	expected := [][]string{{"arg1", "result1", "arg3"}, {"arg1", "result2", "arg3"}}
+
+	result := ReplaceTemplateArgs(args, target, results)
+
+	if len(result) != len(expected) {
+		t.Errorf("expected %v, got %v", expected, result)
+	}
+
+	for i, v := range result {
+		if len(v) != len(expected[i]) {
+			t.Errorf("expected %v, got %v", expected, result)
+		}
+
+		for j, arg := range v {
+			if arg != expected[i][j] {
+				t.Errorf("expected %v, got %v", expected, result)
+			}
 		}
 	}
 }
