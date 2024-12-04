e5a5436c4fbb71bbcc453434e91c8e36624210d4
Luka de Jong <lukadejong2002@gmail.com>
2024-12-04 12:11:42 +0100

    Added logging and added lua error functions

--- genericParserService/lua_parser/__init__.py
+++ genericParserService/lua_parser/__init__.py
@@ -1,3 +1,8 @@
+import json
+import logging
+import sys
+import threading
+
 from lupa import LuaRuntime
 import os
 
@@ -6,27 +11,42 @@ from lua_parser.parser_finder import ParserFinder
 
 class Parser:
     def __init__(self, parser_file: str, parser_input: str, parser_folder: str):
+        self.parser_file = parser_file
         self._parser_input = parser_input
 
         self._lua = LuaRuntime(unpack_returned_tuples=True)
         self._lua.eval("function() python = nil; end")()
+        self._lua.globals().report_error = self._report_lua_parser_error
+        self._lua.globals().report_warning = self._report_lua_parser_warning
+        self._lua.globals().panic = self._lua_panic
 
-        with open(os.path.join(parser_folder.rstrip("/"), parser_file),
+        with open(os.path.join(parser_folder.rstrip("/"), self.parser_file),
                   "r", encoding="utf-8") as f:
             self._parser_func = self._lua.eval(f.read())
 
         self._result = None
-
+        self._panic = False
+        self._thread_id = None
 
     @property
     def result(self):
         return self._result
 
+    @property
+    def panicked(self):
+        return self._panic
 
     def parse(self):
-        self._result = self._parse_to_dict(
-            self._parser_func(self._parser_input))
+        self._thread_id = threading.current_thread().native_id
+        result = self._parser_func(self._parser_input)
+        if os.getenv(f"_PARSER_LUA_PANIC_{self._thread_id}", "0") == "1":
+            self._panic = True
+            return
+        self._result = self._parse_to_dict(result)
 
+    def cleanup(self):
+        if f"_PARSER_LUA_PANIC_{self._thread_id}" in os.environ.keys():
+            os.environ.pop(f"_PARSER_LUA_PANIC_{self._thread_id}")
 
     @staticmethod
     def _parse_to_dict(lua_table):
@@ -34,3 +54,29 @@ class Parser:
         for value in lua_dict.values():
             lua_list.append(dict(value))
         return lua_list
+
+    @staticmethod
+    def _report_lua_parser_error(*args) -> None:
+        logger = logging.getLogger("lua_error_reporter")
+        for arg in args:
+            logger.error(arg)
+
+    @staticmethod
+    def _report_lua_parser_warning(*args) -> None:
+        logger = logging.getLogger("lua_error_reporter")
+        for arg in args:
+            logger.warning(arg)
+
+    @staticmethod
+    def _lua_panic(*args) -> None:
+        thread_id = threading.current_thread().native_id
+        os.environ[f"_PARSER_LUA_PANIC_{thread_id}"] = "1"
+        logger = logging.getLogger("lua_panic_reporter")
+        logger.propagate = False
+        formatter = logging.Formatter(
+            '{"time": "%(asctime)s", "logger_name": "%(name)s", "level": "%(levelname)s", "messages": %(message)s}'
+        )
+        handler = logging.StreamHandler(stream=sys.stderr)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.error(json.dumps(args))
--- genericParserService/main.py
+++ genericParserService/main.py
@@ -1,3 +1,6 @@
+import logging
+import sys
+
 from lua_parser import Parser, ParserFinder
 from dotenv import load_dotenv
 import argparse
@@ -7,7 +10,7 @@ import os
 
 load_dotenv()
 
-PARSER_FOLDER = os.getenv('PARSER_FOLDER')
+PARSER_FOLDER = os.getenv("PARSER_FOLDER")
 if PARSER_FOLDER is None:
     raise ValueError("PARSER_FOLDER environment variable is not set")
 if not os.path.exists(PARSER_FOLDER):
@@ -20,8 +23,18 @@ parser.add_argument("target",
                     help="""Which parser file(s) to use.
                     You can specify either a file or directory""")
 parser.add_argument("input", help="STDOUT of the tools that you want to parse")
+parser.add_argument("-v", "--verbose", action="store_true",
+                    help="Enable verbose output, aka loglevel DEBUG")
 
 args = parser.parse_args()
+
+
+logging.basicConfig(
+    level=logging.DEBUG if args.verbose else logging.INFO,
+    format='{"time": "%(asctime)s", "logger_name": "%(name)s", "level": "%(levelname)s", "message": "%(message)s"}',
+    stream=sys.stderr
+)
+
 
 pf = ParserFinder(args.target, PARSER_FOLDER)
 
@@ -31,6 +44,7 @@ for parser_file in pf.yield_parsers():
     thread = threading.Thread(target=lua_parser.parse)
     lua_parser_threads.append({"thread": thread, "parser": lua_parser})
     thread.start()
+    logging.debug("Started a thread with id %s", thread.native_id)
 
 thread_active = [True for _ in range(len(lua_parser_threads))]
 while True in thread_active:
@@ -39,8 +53,18 @@ while True in thread_active:
             thread_active[i] = False
 
 results = []
+panicked_files = []
 for thread in lua_parser_threads:
-    results += thread["parser"].result
+    thread["parser"].cleanup()
+    result = thread["parser"].result
+    panicked = thread["parser"].panicked
+    if panicked:
+        panicked_files.append(thread["parser"].parser_file)
+    elif result:
+        results.append(result)
+
+if len(panicked_files) > 0:
+    logging.error('{"panicked_parsers": %s', json.dumps(panicked_files))
 
 print(json.dumps({
     "name": args.name,
