84ba3d2f9c47b7329e92b1c327bcc7c1eee5fc56
Luka de Jong <lukadejong2002@gmail.com>
2024-12-09 13:19:15 +0100

    Fixed pylint errors

--- decodyService/endpoints/load.py
+++ decodyService/endpoints/load.py
@@ -5,7 +5,7 @@ import json
 import os
 
 from helpers import safe_eval, Database
-from helpers.types import LoadEndpointInputFormat, DecodyDatabaseRuleFormat, DecodyOutputResultFormat
+from helpers.types import LoadEndpointInputFormat, DecodyDatabaseRuleFormat, DecodyDatabaseResultFormat
 
 load_app = Blueprint("load_app", __name__)
 logger = logging.getLogger(__name__)
@@ -32,17 +32,21 @@ def load_endpoint(request_id: str) -> tuple[str, int]:
     try:
         jsonschema.validate(instance=request_body, schema=schema)
     except jsonschema.ValidationError as e:
-        logger.error("Validation failed, body not properly formatted", e)
+        logger.error(
+            "Validation failed, body not properly formatted with error: %s",
+            e
+        )
         return "Body not properly formatted", 400
 
     # Get all input objects and check if the request body is a duplicate
     aggregated_input_str = Database.KeyStorage.get(f"{request_id}-input")
-    aggregated_input: list[LoadEndpointInputFormat] = json.loads(aggregated_input_str) \
-        if aggregated_input_str else []
+    aggregated_input: list[LoadEndpointInputFormat] = json.loads(
+        aggregated_input_str) if aggregated_input_str else []
     for a_input in aggregated_input:
         if a_input == request_body:
             logger.info(
-                "Request body already exists in aggregated input, returning early.")
+                "Request body already exists in aggregated input,"
+                " returning early.")
             return "Duplicate request", 409
     aggregated_input.append(request_body)
     Database.KeyStorage.set(
@@ -69,10 +73,10 @@ def load_endpoint(request_id: str) -> tuple[str, int]:
                          "long": result["long"],
                          "scanner_name": request_body["scanner_name"]
                      }):
-                logger.debug(f"{rule["condition"]} did not evaluate")
+                logger.debug("%s did not evaluate", {rule["condition"]})
                 continue
 
-            aggregated_results.append(DecodyOutputResultFormat(
+            aggregated_results.append(DecodyDatabaseResultFormat(
                 category=rule["category"],
                 rule_name=rule["name"],
                 rule_explanation=rule["explanation"],
