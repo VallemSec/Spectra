d228d988927c3971190cc41b1175124173b43800
Fay <61050421+FayKn@users.noreply.github.com>
2025-01-20 10:23:40 +0100

    Update main.go

--- orchestratorService/main.go
+++ orchestratorService/main.go
@@ -64,7 +64,7 @@ func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
